a0f77e1ccc0ef8ce0729e65b7b88de534b9a0ce8
popofffnik <57560494+popofffnik@users.noreply.github.com>
2022-12-23 20:10:10 +0300

    Добавьте файлы проекта.

---
+++ Laba 7.4/Laba 7.4.cpp
@@ -0,0 +1,81 @@
+﻿#include "settings.h"
+
+
+
+using namespace std;
+
+
+
+void printVector(vector<float> vec);
+
+void addVectorEl(vector<float>& vec, int index, int value);
+
+
+
+map<int, int> Settings::mp;
+
+
+
+int main() {
+
+    vector<float> vec = { 1, 2, 3, 4, 5 };
+
+    printVector(vec);
+
+    addVectorEl(vec, 3, 20);
+
+    printVector(vec);
+
+    vec.pop_back();
+
+    printVector(vec);
+
+
+
+    cout << "____________________\n\n";
+
+
+
+
+
+    Settings::add(1, 20);
+
+    Settings::add(5, 8);
+
+    Settings::add(3, 13);
+
+    cout << Settings::get(5) << endl;
+
+    Settings::print();
+
+}
+
+
+
+void printVector(vector<float> vec) {
+
+    for (int i = 0; i < vec.size(); i++) {
+
+        cout << vec.at(i) << ' ';
+
+    }
+
+    cout << endl;
+
+}
+
+
+
+void addVectorEl(vector<float>& vec, int index, int value) {
+
+    vector<float>::iterator it = vec.begin();
+
+    for (int i = 0; i < index; i++) {
+
+        it++;
+
+    }
+
+    vec.insert(it, value);
+
+}
